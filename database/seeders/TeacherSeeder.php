26df32d4f36e00064f00aeb1b10817c3b359eb75
AshrafUzzaman04 <ashraf.uzzaman04082004@gmail.com>
2023-10-25 02:44:31 +0600

    seeders

---
+++ database/seeders/TeacherSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class TeacherSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // DB::table('teachers')->insert([
+        //     'name' => Str::random(10),
+        //     'email' => Str::random(10) . '@gmail.com',
+        //     'phone' => 0 . rand(1111111111, 9999999999),
+        // ]);
+        $data = array([
+            "name" => str::random(10),
+            "email" => str::random(10),
+            "phone" => 0 . rand(1111111111, 9999999999),
+        ], [
+            "name" => str::random(10),
+            "email" => str::random(10),
+            "phone" => 0 . rand(1111111111, 9999999999),
+        ], [
+            "name" => str::random(10),
+            "email" => str::random(10),
+            "phone" => 0 . rand(1111111111, 9999999999),
+        ], [
+            "name" => str::random(10),
+            "email" => str::random(10),
+            "phone" => 0 . rand(1111111111, 9999999999),
+        ], [
+            "name" => str::random(10),
+            "email" => str::random(10),
+            "phone" => 0 . rand(1111111111, 9999999999),
+        ]);
+
+        DB::table("teachers")->insert($data);
+    }
+}
